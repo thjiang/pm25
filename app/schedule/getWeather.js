0c9c823eb9e24b3ac90b92a670664773474b2896
thjiang <browserify@qq.com>
2018-12-02 21:47:10 +0800

    schedule

--- app/schedule/getWeather.js
+++ app/schedule/getWeather.js
@@ -1,15 +1,45 @@
+const http = require("http");
+const cheerio = require("cheerio");
+
 module.exports = {
     schedule: {
-        interval: '5h',
+        interval: '24h',
         type: 'worker',
         immediate: true
     },
     async task(ctx) {
-        // const res = await ctx.curl('http://www.api.com/cache', {
-        //     dataType: 'json',
-        // });
-        // ctx.app.cache = res.data;
-        // ctx.service.user.get(1234);
+        const baseUrl = "http://www.pm25.com/rank.html";
+
+        let html = "";
+        let results = [];
+
+        http.get(baseUrl, function (res) {
+            res.on("data", function (chunk) {
+                html += chunk;
+            })
+            res.on("end", function () {
+                var $ = cheerio.load(html);
+                $(".rank_box li").each((i, item) => {
+                    if (
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "杭州" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "大庆" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "成都" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "洛阳" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "北京" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "重庆" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "苏州" ||
+                        $(".rank_box .pjadt_location")[i].childNodes[0].data === "西安"
+                    ) {
+                        results.push({
+                            location: $(".rank_box .pjadt_location")[i].childNodes[0].data,
+                            aqi: $(".rank_box .pjadt_aqi")[i].childNodes[0].data,
+                            quality: $(".rank_box .pjadt_quality")[i].childNodes[0].childNodes[0].data
+                        });
+                    }
+                })
+                console.log(results);
+            });
+        });
         console.log(new Date());
     },
 };
